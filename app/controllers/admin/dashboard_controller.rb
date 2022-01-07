1a1de9657daed2f55223c5414b04da638e1df3f5
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-06 23:25:52 -0500

    set instance variable in controller to fetch product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
 
   def show
+    @category_count = Category.count(:all)
+    @product_count = Product.count(:all)
   end
+  
 end
