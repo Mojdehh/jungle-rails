8a374da4fbd30d4c752b0e33c2fc8c6ba414100e
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-09 18:47:19 -0500

    add comments to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # new methods to look up the user, if they're logged in, and save their user object to a variable called @current_user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  # allows us to use @current_user in our view files
   helper_method :current_user
 
   def authorize
     redirect_to '/login' unless current_user
   end
-  
+
   private
 
   def cart
