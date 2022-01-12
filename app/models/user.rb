483a7f2202595d436fa116f0d35832ba24d58fae
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-11 22:05:04 -0500

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true, length: { minimum: 3 }
+
 end
