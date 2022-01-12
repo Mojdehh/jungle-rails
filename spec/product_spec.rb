ff4c4cf1df8ce51364bd6171fcacb7718464042b
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-11 21:12:42 -0500

    create test to not save without a name

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -14,6 +14,19 @@ RSpec.describe Product, type: :model do
       expect(@product).to be_valid
       expect(@product.errors.full_messages).to be_empty
     end
+
+    it "should not save without a name" do
+      @category = Category.create(name: 'Shoes')
+      @product = Product.new(
+        name: nil,
+        price_cents: 8000,
+        quantity: 12,
+        category_id: @category.id
+      )
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Name can't be blank"
+    end
+
   end
 
 end
