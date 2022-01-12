b58de69c362dbef17c705ec836550fb4d55b2ce6
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-11 21:01:41 -0500

    create an example to ensure a product is saved with all four attributes

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,5 +1,18 @@
+require 'rails_helper'
+
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+
+    it "should save a valid product" do
+      @category = Category.create(name: 'Shoes')
+      @product = Product.new(
+        name: "Chic",
+        price_cents: 8000,
+        quantity: 12,
+        category_id: @category.id
+      )
+      expect(@product).to be_valid
+    end
   end
+
 end
