5b2766b6a83088b84a9fc34bcff25944d4d2cd2f
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-12 19:36:08 -0500

    write test to check if user can see product detail page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitors can navigate from home page to product detail page", type: :feature, js: true do
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+
+    scenario "by clicling on a product" do
+      visit root_path
+
+      click_link('Details', match: :first)
+      expect(page).to have_text(/description/i)
+      save_screenshot
+
+    end
 end
